2ba6ac451d1c764335460854f9d9853338eb0c45
Zmeev06 <88145578+Zmeev06@users.noreply.github.com>
2022-10-23 12:29:03 +0300

    admin panel

--- Back/main.go
+++ Back/main.go
@@ -1,30 +1,34 @@
 package main
 
 import (
-	"./index"
-	"./login"
-	"./reg"
-	"./user_page"
-	"github.com/gorilla/mux"
-	"net/http"
+  "./admin"
+  "./index"
+  "./login"
+  "./reg"
+  "./user_page"
+  "github.com/gorilla/mux"
+  "net/http"
 )
 
 func handleFunc() {
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
-	rtr := mux.NewRouter()
-	rtr.HandleFunc("/", index.Index)
-	rtr.HandleFunc("/login", login.Login)
-	rtr.HandleFunc("/userlogin", login.UserLogin)
-	rtr.HandleFunc("/registration", reg.RegPage)
-	rtr.HandleFunc("/newuser", reg.Reg)
-	rtr.HandleFunc("/reg_err", reg.RegErr)
-	rtr.HandleFunc("/user/s/{id:[0-9]+}", user_page.UserSPage)
-	rtr.HandleFunc("/user/p/{id:[0-9]+}", user_page.UserPPage)
 
-	http.Handle("/", rtr)
-	http.ListenAndServe(":8080", nil)
+  rtr := mux.NewRouter()
+  rtr.HandleFunc("/", index.Index)
+  rtr.HandleFunc("/login", login.Login)
+  rtr.HandleFunc("/userlogin", login.UserLogin)
+  rtr.HandleFunc("/registration", reg.RegPage)
+  rtr.HandleFunc("/newuser", reg.Reg)
+  rtr.HandleFunc("/reg_err", reg.RegErr)
+  rtr.HandleFunc("/log_err", login.LogErr)
+  rtr.HandleFunc("/student/{id:[0-9]+}", user_page.UserSPage)
+  rtr.HandleFunc("/prof/{id:[0-9]+}", user_page.UserPPage)
+  rtr.HandleFunc("/admin", admin.AdminPage)
+
+  http.Handle("/", rtr)
+  http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+  http.ListenAndServe(":8080", nil)
 }
 
 func main() {
-	handleFunc()
+  handleFunc()
 }
