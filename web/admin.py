d6491634ae90652bb4a609bad8c2edb6fb64f272
loxigl <43906792+loxigl@users.noreply.github.com>
2022-10-24 00:06:27 +0300

    backend

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from web.models import User, Meta
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Meta)
--- web/models.py
+++ web/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+from django.urls import reverse
+
+
+class User(models.Model):
+  id = models.AutoField(primary_key=True)
+  name = models.CharField(max_length=150)
+  email = models.EmailField
+  password = models.CharField
+  who = models.IntegerField
+
+  def __str__(self):
+    return self.name
+
+  def get_email(self):
+    return self.email
+
+  def get_password(self):
+    return self.password
+
+  def get_absolute_url(self):
+    return reverse('user', args=[str(self.id)])
+
+
+record = User(name='bruh')
+record.save()
+
+
+class Meta:
+  ordering = ['id']
