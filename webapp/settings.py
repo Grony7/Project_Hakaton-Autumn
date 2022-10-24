73b01a95aaabaf1d8f51ece9efe84a3ddb1bf6c2
loxigl <43906792+loxigl@users.noreply.github.com>
2022-10-24 15:15:03 +0300

    autoreload and static

--- webapp/settings.py
+++ webapp/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
   'django.contrib.sessions',
   'django.contrib.messages',
   "django_browser_reload",
-  'django.contrib.staticfiles',
   'web.apps.WebConfig',
 ]
 
@@ -128,6 +127,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR / 'build/static')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
